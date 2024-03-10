9c6af6cc55cd90113d8cd2984b0ba9a1931ff6f8
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-03-10 19:49:20 +0100

    m

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
 
+const upload = multer({ dest: 'uploads' });
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to Keleidoscope API</h1>');
+  // res.render('index.ejs');
+  res.json({ msg: 'Welcome to Keleidoscope API' });
+});
+
+app.post('/', upload.single('file'), (req, res) => {
+  console.log(req.file);
+  res.json({ msg: 'file uploaded successfully' });
 });
 
 const server = () => {
