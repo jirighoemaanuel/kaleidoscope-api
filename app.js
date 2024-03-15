d3846af89f94127454b2928d5acd7b8d91156618
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-03-16 00:20:13 +0100

    m

--- app.js
+++ app.js
@@ -1,43 +1,17 @@
 import express from 'express';
-import multer from 'multer';
-import path from 'path';
-import mime from 'mime-types';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+import files from './routes/files.js';
+
+// Middleware
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.json());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+app.use('/kaleidoscope/api/v1/files', files);
 
-const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  const fileId = req.file.filename;
-  res.json({ msg: fileId });
-});
-
-app.get('/file/:fileId', (req, res) => {
-  const fileId = req.params.fileId;
-  const filePath = `uploads/${fileId}`;
-  const resolvedPath = path.resolve(filePath);
-  const mimeType = mime.lookup(resolvedPath);
-  res.setHeader('Content-Type', mimeType);
-  res.sendFile(resolvedPath, (err) => {
-    if (err) {
-      console.error(`Error sending file: ${err}`);
-      res.status(500).send('Error sending file');
-    }
-  });
-});
 
 const server = () => {
   try {
--- routes/files.js
+++ routes/files.js
@@ -1,7 +1,38 @@
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import mime from 'mime-types';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-  res.send('Welcome Alien');
+router.route('/file/:fileId').get((req, res) => {
+  const fileId = req.params.fileId;
+  const filePath = `uploads/${fileId}`;
+  const resolvedPath = path.resolve(filePath);
+  const mimeType = mime.lookup(resolvedPath);
+  res.setHeader('Content-Type', mimeType);
+  res.sendFile(resolvedPath, (err) => {
+    if (err) {
+      console.error(`Error sending file: ${err}`);
+      res.status(500).send('Error sending file');
+    }
+  });
 });
+
+router.route('/upload').post(upload.single('file'), (req, res) => {
+  const fileId = req.file.filename;
+  res.json({ msg: fileId });
+});
+
+export default router;
