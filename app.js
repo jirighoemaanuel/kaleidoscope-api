f06ca68df830bda00d4fcbefc20b928e8ab76590
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-04-06 21:35:59 +0100

    m

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(express.json());
 // Route
 app.use('/kaleidoscope/api/v1/files', files);
 
+app.get('/', (req, res) => {
+  res.json({
+    msg: 'Welcome to KaleidoscopeAPI. Make call to endpoint: /kaleidoscope/api/v1/files',
+  });
+});
+
 const server = () => {
   try {
     app.listen(port, () => {
