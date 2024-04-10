196a5495caf1896fbb87a748921a14e184595bc1
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-04-10 23:22:08 +0100

    update

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+import User from '../models/User.js';
+import { StatusCodes } from 'http-status-codes.js';
+import { BadRequestError } from '../errors/index.js';
+
+export const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
+
+export const login = async (req, res) => {
+  res.send('login user');
+};
